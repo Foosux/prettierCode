327e5fb6ffec77a269ab3110aac286d8d0d770eb
zhumin <zhumin@lianjia.com>
2020-03-16 08:29:25 +0800

    :bookmark: 支持ES语法的最精简版本

--- FE/.eslintrc.js
+++ FE/.eslintrc.js
@@ -4,21 +4,14 @@ module.exports = {
     es6: true
   },
   extends: [
-    // eslint默认检测规则（非必选）
+    // eslint默认检测规则（非必选，可挑选使用其它大厂规则）
     'eslint:recommended', 
-    // React检测规则（React项目必选）
-    'plugin:react/recommended', 
-    // Prettier检测规则，修复与eslint检测重复的问题（必选）    
+    // Prettier检测规则，修复与eslint检测重复的问题（建议选择）    
     'plugin:prettier/recommended'
   ],
-  globals: {
-  },
   parserOptions: {
-    ecmaFeatures: {
-      // 开启 .jsx 文件的检测。配合 【eslint --ext .jsx ./】 指令使用。
-      jsx: true
-    },
-    // 制定ES版本，若开启了 evn_es6_true 则默认为 6
+    // 制定解析ES的版本，若开启了 evn_es6_true 则默认为 6
+    // 2009/ES5 2015/ES6 2016/ES7 2017/ES8 2018/ES9 2019/ES10
     ecmaVersion: 2019,
     // import & export 等模块化语法必须
     sourceType: 'module'
